230c011b6b97ac68cb97c95d55e36dd1376e40bc
Rajnish Patel <44401319+rajnish17100@users.noreply.github.com>
2019-03-13 00:02:18 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 #create a database with name profile 
 CREATE DATABASE profile;
 
-#create  table named as admin
+#create  table named as admin to register the admin of this  website
 
 CREATE TABLE `profile`.`admin` ( 
   `id` INT(5) NOT NULL AUTO_INCREMENT ,
@@ -12,7 +12,7 @@ CREATE TABLE `profile`.`admin` (
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
-#create table named as jobpost
+#create table named as jobpost to store job posts created  by recruiters 
 
 CREATE TABLE `profile`.`jobpost` (
   `id` INT(5) NOT NULL AUTO_INCREMENT ,
@@ -21,5 +21,15 @@ CREATE TABLE `profile`.`jobpost` (
   `title` VARCHAR(255) NOT NULL ,
   `body` TEXT NOT NULL ,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+
+#create table named as poregister  to register the placement officer
+CREATE TABLE `profile`.`poregister`( 
+  `id` INT(5) NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(255) NOT NULL ,
+  `email` VARCHAR(255) NOT NULL ,
+  `password` VARCHAR(32) NOT NULL , 
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
