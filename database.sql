5c782b59b69296ff30e98948bf312224d4c81f21
Rajnish Patel <44401319+rajnish17100@users.noreply.github.com>
2019-03-12 23:52:28 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,13 @@
+#create a database with name profile 
 CREATE DATABASE profile;
+
+#create  table named as admin
+
+CREATE TABLE `profile`.`admin` ( 
+  `id` INT(5) NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(255) NOT NULL ,
+  `email` VARCHAR(255) NOT NULL ,
+  `image` VARCHAR(255) NOT NULL ,
+  `password` VARCHAR(32) NOT NULL ,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
