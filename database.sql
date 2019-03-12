e841f0f4125c57d5add5f917b9f98a022b09325e
Rajnish Patel <44401319+rajnish17100@users.noreply.github.com>
2019-03-12 23:57:35 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -11,3 +11,15 @@ CREATE TABLE `profile`.`admin` (
   `password` VARCHAR(32) NOT NULL ,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
+
+#create table named as jobpost
+
+CREATE TABLE `profile`.`jobpost` (
+  `id` INT(5) NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(255) NOT NULL ,
+  `companyname` VARCHAR(255) NOT NULL ,
+  `title` VARCHAR(255) NOT NULL ,
+  `body` TEXT NOT NULL ,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
